f1488a74fd92fe8b6de254b684f404f505e006da
AlexWestarb <88094496+AlexWestarb@users.noreply.github.com>
2024-05-13 20:37:31 -0300

    Listagem controle

--- scr/main.py
+++ scr/main.py
@@ -1,19 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, session
 from settings import HOST, PORT, DEBUG
+import os
 
 # import blueprint criado
 from mod_index.index import bp_index
 from mod_funcionario.funcionario import bp_funcionario
 from mod_cliente.cliente import bp_cliente
 from mod_produto.produto import bp_produto
+from mod_login.login import bp_login
+from mod_erro.erro import bp_erro
 
 app = Flask(__name__)
+
+# gerando uma chave randômica para secret_key
+app.secret_key = os.urandom(12).hex()
 
 # registro das rotas do blueprint
 app.register_blueprint(bp_index)
 app.register_blueprint(bp_funcionario)
 app.register_blueprint(bp_cliente)
 app.register_blueprint(bp_produto)
+app.register_blueprint(bp_login)
+app.register_blueprint(bp_erro)
+
+# ajuste SAMESITE
+app.config.update(
+    SESSION_COOKIE_SAMESITE='None',
+    SESSION_COOKIE_SECURE='True'
+)
+
+@app.route('/')
+def index():
+    return render_template('formLogin.html')
 
 if __name__ == "__main__":
     """ Inicia o aplicativo WEB Flask """
